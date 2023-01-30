399b619353072e8d9544e1f537c2a2677a8da535
Cameron Omiccioli <caomicc@gmail.com>
2023-01-30 18:39:05 -0500

    chore: ugggghhh

--- src/ui/theme/index.ts
+++ src/ui/theme/index.ts
@@ -4,15 +4,15 @@ import { extendTheme } from '@chakra-ui/react';
 import colors from './colors';
 import fonts from './fonts';
 
-const colorMode: ThemeConfig = {
-  initialColorMode: 'dark',
+const config: ThemeConfig = {
+  initialColorMode: 'dark', // 'dark' | 'light'
   useSystemColorMode: false,
 };
 
 const customTheme = extendTheme({
   fonts,
   colors,
-  colorMode,
+  config,
   // components: {
   //   Button,
   // },
