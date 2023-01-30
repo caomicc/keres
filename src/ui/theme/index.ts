2c30ec98d02a8b4e08107dcede49100923531442
Cameron Omiccioli <caomicc@gmail.com>
2023-01-30 18:27:55 -0500

    chore: color mode

--- src/ui/theme/index.ts
+++ src/ui/theme/index.ts
@@ -3,9 +3,15 @@ import { extendTheme } from '@chakra-ui/react';
 import colors from './colors';
 import fonts from './fonts';
 
+const colorMode = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
 const customTheme = extendTheme({
   fonts,
   colors,
+  colorMode,
   // components: {
   //   Button,
   // },
